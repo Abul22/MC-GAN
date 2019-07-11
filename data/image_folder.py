3360cc04b40b932d28b52b823c32f85ec8c4acb1
Abul22 <luke.luklus@gmail.com>
2019-07-11 13:46:33 +1000

    sadsa
    asd

--- data/image_folder.py
+++ data/image_folder.py
@@ -27,7 +27,7 @@ def is_image_file(filename):
 def make_dataset(dir):
     images = []
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
-
+    print(dir)
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
             print(8888888)
